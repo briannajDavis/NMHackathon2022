99956753b97e4f76ba9b1f2fa64f0b6d9f10c04c
Brianna Davis <briannadavis@northwesternmutual.com>
2022-08-02 13:20:18 -0500

    fixes

--- application/src/App.js
+++ application/src/App.js
@@ -1,15 +1,17 @@
 import "./App.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
-import React from 'react'
+import React from "react";
 function App() {
   const [visible, setVisible] = useState(0);
   return (
     <div className="App">
       {visible === 0 && (
         <div id="signIn">
-          <h2>Welcome to A NAME WE HAVENT CHOSEN YET</h2>
-          <h4>Sign in or sign up below</h4>
+          <h1>Welcome to NM Lunch Meet</h1>
+          <h4>Have lunch with like minded individuals, across departments.
+            <br></br>Meet new people, explore NM.
+            </h4>
           <div id="buttons">
             <button onClick={() => setVisible(-1)}>Sign In</button>
             <button onClick={() => setVisible(1)}>Sign Up</button>
@@ -19,10 +21,10 @@ function App() {
       {visible === -1 && (
         <div className="signOn">
           <button onClick={() => setVisible(0)}>Back</button>
-          <input id="fName" type="text" placeholder="First Name"></input>
-          <input id="lName" type="text" placeholder="Last Name"></input>
+          <input className="fName" type="text" placeholder="First Name"></input>
+          <input className="lName" type="text" placeholder="Last Name"></input>
           <button>
-            <Link className="links" to={{ pathname: "/myProfile"}}>
+            <Link className="links" to={{ pathname: "/myProfile" }}>
               Enter
             </Link>
           </button>
@@ -31,10 +33,10 @@ function App() {
       {visible === 1 && (
         <div className="signOn">
           <button onClick={() => setVisible(0)}>Back</button>
-          <input id="fName" type="text" placeholder="First Name"></input>
+          <input className="fName" type="text" placeholder="First Name"></input>
           <input id="lName" type="text" placeholder="Last Name"></input>
           <button>
-            <Link className="links" to={{ pathname: "/survey"}}>
+            <Link className="links" to={{ pathname: "/survey" }}>
               Enter
             </Link>
           </button>
